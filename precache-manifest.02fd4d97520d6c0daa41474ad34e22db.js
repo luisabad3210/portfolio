391b7c8c3cfe19662c858dcbd49d9ed531926420
luisabad84 <luisabad84@gmail.com>
2020-03-02 18:22:48 -0500

    Updates

---
+++ precache-manifest.02fd4d97520d6c0daa41474ad34e22db.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d7bc7300cb7e2d9d4bc1e84f9bd02d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5140a708b81356d65387",
+    "url": "/portfolio/static/css/main.df58443f.chunk.css"
+  },
+  {
+    "revision": "656c0f183429cf8b0c36",
+    "url": "/portfolio/static/js/2.00b59433.chunk.js"
+  },
+  {
+    "revision": "5140a708b81356d65387",
+    "url": "/portfolio/static/js/main.d9bbe873.chunk.js"
+  },
+  {
+    "revision": "a694c17334d77ca95504",
+    "url": "/portfolio/static/js/runtime-main.eaf37d0e.js"
+  },
+  {
+    "revision": "73588ccef59544308ec50fc2b37f9a29",
+    "url": "/portfolio/static/media/ProjectOne.73588cce.png"
+  },
+  {
+    "revision": "694715a77fc3ebb644d47835f7c57a22",
+    "url": "/portfolio/static/media/ProjectTwo.694715a7.png"
+  },
+  {
+    "revision": "368355e28e91d37df1daa4c47294b2bb",
+    "url": "/portfolio/static/media/background.368355e2.jpg"
+  },
+  {
+    "revision": "07270c13412737933c6f3468d97618ad",
+    "url": "/portfolio/static/media/css.07270c13.png"
+  },
+  {
+    "revision": "0ea316dbe0fd5dab21e160b3f48fa6b5",
+    "url": "/portfolio/static/media/html.0ea316db.png"
+  },
+  {
+    "revision": "378c80aef4b3e4cbd2edb407c79a361e",
+    "url": "/portfolio/static/media/image1.378c80ae.png"
+  },
+  {
+    "revision": "c66ad30b55d9395fc0413f0f14bd9730",
+    "url": "/portfolio/static/media/node.c66ad30b.jpg"
+  },
+  {
+    "revision": "63fe1bd93509f9065fc5b75c7ed966b3",
+    "url": "/portfolio/static/media/react.63fe1bd9.png"
+  },
+  {
+    "revision": "911d87d7c7b792f930e7f0a46b9994f0",
+    "url": "/portfolio/static/media/uiux.911d87d7.png"
+  }
+]);
